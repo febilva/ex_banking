2d37c2d1b7da57c47f974634996e93ff7b539c64
Febil <febil.tvr@gmail.com>
2022-02-04 15:39:06 +0530

    feat:  add deposit option for user

--- lib/ex_banking/user.ex
+++ lib/ex_banking/user.ex
@@ -2,17 +2,55 @@ defmodule ExBanking.User do
   use GenServer
 
   @impl true
-  def init(user) do
-    {:ok, user}
+  def init(_user) do
+    {:ok, %{}}
   end
 
   def create(user) do
-    case GenServer.start_link(__MODULE__, [], name: {:via, Registry, {ExBanking.UserRegistry, user}}) do
+    case GenServer.start_link(__MODULE__, [],
+           name: {:via, Registry, {ExBanking.UserRegistry, user}}
+         ) do
       {:ok, _pid} ->
         :ok
 
       {:error, {:already_started, _pid}} ->
         {:error, :user_already_exists}
     end
+  end
+
+  # find the user pid to check wheather the user exists
+  # if the user exists check the number of requests
+  # depost the amount
+  def deposit(user, amount, currency) do
+    with pid when is_pid(pid) <- user_exists?(user) do
+      {:message_queue_len, mailbox_len} = Process.info(pid, :message_queue_len)
+
+      if mailbox_len <= 10 do
+        GenServer.call(pid, {:deposit, amount, currency})
+      else
+        {:error, :too_many_requests_to_user}
+      end
+    else
+      nil ->
+        {:error, :user_does_not_exist}
+    end
+  end
+
+  def user_exists?(user) do
+    user
+    |> user_pid()
+    |> GenServer.whereis()
+  end
+
+  def user_pid(user) do
+    {:via, Registry, {ExBanking.UserRegistry, user}}
+  end
+
+  @impl GenServer
+  def handle_call({:deposit, amount, currency}, _from, balances) do
+    balance = Map.get(balances, currency, Decimal.new(0))
+    {:ok, amount} = Decimal.cast(amount)
+    new_balance = Decimal.add(balance, amount)
+    {:reply, {:ok, Decimal.to_float(new_balance)}, Map.put(balances, currency, new_balance)}
   end
 end
--- test/ex_banking_test.exs
+++ test/ex_banking_test.exs
@@ -11,10 +11,41 @@ defmodule ExBankingTest do
       assert ExBanking.create_user("") == {:error, :wrong_arguments}
       assert ExBanking.create_user("  ") == {:error, :wrong_arguments}
     end
+
     test "should not create a user with the same name" do
       :ok = ExBanking.create_user(@valid_name)
 
       assert ExBanking.create_user(@valid_name) == {:error, :user_already_exists}
+    end
+  end
+
+  describe "deposit/3" do
+    @user "febil"
+    test "will credit the given amount to the user" do
+      :ok = ExBanking.create_user(@user)
+
+      assert ExBanking.deposit(@user, 0.1, "USD") == {:ok, 0.1}
+      assert ExBanking.deposit(@user, 0.2, "USD") == {:ok, 0.3}
+    end
+
+    test "should not deposit with invalid values" do
+      :ok = ExBanking.create_user(@user)
+
+      assert ExBanking.deposit("", 200.00, "USD") == {:error, :wrong_arguments}
+      assert ExBanking.deposit(@user, 200.001, "USD") == {:error, :wrong_arguments}
+      assert ExBanking.deposit(@user, 200.00, "") == {:error, :wrong_arguments}
+      assert ExBanking.deposit(@user, 200.00, "    ") == {:error, :wrong_arguments}
+    end
+
+    test "shouldnt allow the deposit if the user doesnt exists" do
+      assert ExBanking.deposit("user", 200.00, "USD") == {:error, :user_does_not_exist}
+    end
+
+    test "shouldnt allow more than 10 requests at same time" do
+      :ok = ExBanking.create_user(@user)
+      pids = for _ <- 1..200, do: Task.async(fn -> ExBanking.deposit(@user, 200.00, "USD") end)
+
+      assert {:error, :too_many_requests_to_user} in Task.await_many(pids)
     end
   end
 end
